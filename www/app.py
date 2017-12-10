8c46ababab24a3af2350e244f0cdd405ec005fc0
封枫 <funny@192.168.2.100>
2017-12-10 20:56:29 +0800

    创建webapp骨架

--- www/app.py
+++ www/app.py
@@ -20,6 +20,24 @@ def init(loop):
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
