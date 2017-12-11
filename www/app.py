0377ded04f63fb718737ad63280a46300160f866
windend <fengfengwin@yeah.net>
2017-12-11 15:01:45 +0800

    增加select函数

--- www/app.py
+++ www/app.py
@@ -9,14 +9,14 @@ from aiohttp import web
 
 
 def index(request):
-    return web.Response(body='<h1>Awesome</h1>', content_type='text/html')
+    return web.Response(body='<h1>Awesome country little hey jude</h1>', content_type='text/html')
 
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    srv = yield from loop.create_server(app.make_handler(), '192.168.0.188', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
@@ -37,6 +37,23 @@ def create_pool(loop, **kw):
         minsize=kw.get('minsize', 1),
         loop=loop
     )
+
+
+@asyncio.coroutine
+def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    with(yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.excute(sql.replace('?', '%s'), args or())
+        if siz:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows return:%s' % len(rs))
+        return rs
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
