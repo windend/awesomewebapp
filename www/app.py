ddb6a8ebf55511379e94eedb4f549e6947f5b3dc
封枫 <funny@192.168.2.101>
2017-12-12 20:47:23 +0800

    ...

--- www/app.py
+++ www/app.py
@@ -24,54 +24,6 @@ def init(loop):
     return srv
 
 
-# @asyncio.coroutine
-# def create_pool(loop, **kw):
-#     logging.info('create database connection pool...')
-#     global __pool
-#     __pool = yield from aiomysql.create_pool(
-#         host=kw.get('host', 'localhost'),
-#         port=kw.get('port', 3306),
-#         user=kw['user'],
-#         password=kw['password'],
-#         db=kw['db'],
-#         charset=kw.get('charset', 'utf8'),
-#         autocommit=kw.get('autocommit', True),
-#         maxsize=kw.get('maxsize', 10),
-#         minsize=kw.get('minsize', 1),
-#         loop=loop
-#     )
-
-
-# @asyncio.coroutine
-# def select(sql, args, size=None):
-#     log(sql, args)
-#     global __pool
-#     with(yield from __pool) as conn:
-#         cur = yield from conn.cursor(aiomysql.DictCursor)
-#         yield from cur.excute(sql.replace('?', '%s'), args or())
-#         if siz:
-#             rs = yield from cur.fetchmany(size)
-#         else:
-#             rs = yield from cur.fetchall()
-#         yield from cur.close()
-#         logging.info('rows return:%s' % len(rs))
-#         return rs
-
-
-# @asyncio.coroutine
-# def excute(sql, args):
-#     log(sql)
-#     with (yield from __pool) as conn:
-#         try:
-#             cur = yield from conn.cursor()
-#             yield from cur.excute(sql.replace('?', '%s'), args)
-#             affected = cur.rowcount
-#             yield from cur.close()
-#         except Exception as e:
-#             raise
-#         return affected
-
-
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
