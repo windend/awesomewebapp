47bfe5e3e15b43ad160d7128478aa6b3773eb0e1
windend <fengfengwin@yeah.net>
2017-12-13 17:28:02 +0800

    create a model

--- dbtest.py
+++ dbtest.py
@@ -1,28 +1,42 @@
-from sqlalchemy import Column, String, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import Column, String, create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
 
 
-base = declarative_base()
+# base = declarative_base()
 
 
-class actor(base):
-    __tablename__ = 'actor'
-    actor_id = Column(String(20), primary_key=True)
-    first_name = Column(String(20))
+# class actor(base):
+#     __tablename__ = 'actor'
+#     actor_id = Column(String(20), primary_key=True)
+#     first_name = Column(String(20))
 
 
-engine = create_engine(
-    'mysql+mysqlconnector://root:123456@localhost:3306/sakila')
+# engine = create_engine(
+#     'mysql+mysqlconnector://root:123456@localhost:3306/sakila')
 
-DBsession = sessionmaker(bind=engine)
+# DBsession = sessionmaker(bind=engine)
 
 
-session = DBsession()
+# session = DBsession()
 
-actor = session.query(actor).filter(actor.actor_id == '5').one()
+# actor = session.query(actor).filter(actor.actor_id == '5').one()
 
-print('type:', type(actor))
-print('name:', actor.first_name)
+# print('type:', type(actor))
+# print('name:', actor.first_name)
 
-session.close()
+# session.close()
+
+import orm
+from models import User, Blogs, Comment
+
+
+def test():
+    yield from orm.create_pool(user='root', password='123456', database='awesome')
+
+    u = User(name='test', email='test@test.com', password='11111', image='')
+
+    yield from u.save()
+
+for x in test():
+    pass
--- www/app.py
+++ www/app.py
@@ -15,62 +15,12 @@ def index(request):
     return web.Response(body='<h1>Awesome country little hey jude</h1>', content_type='text/html')
 
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '192.168.0.110', 9000)
+    srv = await loop.create_server(app.make_handler(), '192.168.0.110', 9000)
     logging.info('server started at http://localhost:9000...')
     return srv
-
-
-# @asyncio.coroutine
-# def create_pool(loop, **kw):
-#     logging.info('create database connection pool...')
-#     global __pool
-#     __pool = yield from aiomysql.create_pool(
-#         host=kw.get('host', 'localhost'),
-#         port=kw.get('port', 3306),
-#         user=kw['user'],
-#         password=kw['password'],
-#         db=kw['db'],
-#         charset=kw.get('charset', 'utf8'),
-#         autocommit=kw.get('autocommit', True),
-#         maxsize=kw.get('maxsize', 10),
-#         minsize=kw.get('minsize', 1),
-#         loop=loop
-#     )
-
-
-# @asyncio.coroutine
-# def select(sql, args, size=None):
-#     log(sql, args)
-#     global __pool
-#     with(yield from __pool) as conn:
-#         cur = yield from conn.cursor(aiomysql.DictCursor)
-#         yield from cur.excute(sql.replace('?', '%s'), args or())
-#         if siz:
-#             rs = yield from cur.fetchmany(size)
-#         else:
-#             rs = yield from cur.fetchall()
-#         yield from cur.close()
-#         logging.info('rows return:%s' % len(rs))
-#         return rs
-
-
-# @asyncio.coroutine
-# def excute(sql, args):
-#     log(sql)
-#     with (yield from __pool) as conn:
-#         try:
-#             cur = yield from conn.cursor()
-#             yield from cur.excute(sql.replace('?', '%s'), args)
-#             affected = cur.rowcount
-#             yield from cur.close()
-#         except Exception as e:
-#             raise
-#         return affected
-
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
